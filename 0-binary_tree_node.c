a0e3d5c0199c9639c97d23f16ff556917089fb7b
lunarinaa <aydan.akbarova@gmail.com>
2024-01-05 11:21:49 -0800

    betty style

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -8,6 +8,7 @@
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	struct binary_tree_s *newnode;
+
 	newnode = malloc(sizeof(binary_tree_t));
 	if (newnode == NULL)
 		return (NULL);
