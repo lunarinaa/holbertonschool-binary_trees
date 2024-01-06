9ebfc9fa4c56bd68af734cca25b32677dcedede8
lunarinaa <aydan.akbarova@gmail.com>
2024-01-06 03:03:42 -0800

    unused condition deleted

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -8,12 +8,9 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 	if (node == NULL || node->parent->parent == NULL || node->parent == NULL)
 		return (NULL);
-	if (node->parent->parent->left == NULL || node->parent->parent->right == NULL)
-		return (NULL);
 	if (node->parent == node->parent->parent->left)
-	{
+
 		return (node->parent->parent->right);
-	}
 	else
 		return (node->parent->parent->left);
 }
