703478bc47a21002af849d16e7af786497cdcc98
lunarinaa <aydan.akbarova@gmail.com>
2024-01-06 04:18:29 -0800

    conditional statement added

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -46,6 +46,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		return (0);
 	left_h = binary_tree_height(tree->left);
 	right_h = binary_tree_height(tree->right);
-	return (binary_tree_is_full(tree) && (left_h == right_h));
+	if (binary_tree_is_full(tree) && (left_h == right_h))
+		return (1);
+	return (0);
 }
 
