1db86812dff626d2c9bdb34e468e615f449a45e7
lunarinaa <aydan.akbarova@gmail.com>
2024-01-06 03:09:02 -0800

    updated code

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -9,9 +9,7 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 	if (node == NULL || node->parent->parent == NULL || node->parent == NULL)
 		return (NULL);
 	if (node->parent == node->parent->parent->left)
-
 		return (node->parent->parent->right);
-	else
-		return (node->parent->parent->left);
+	return (node->parent->parent->left);
 }
 
