927ed3ab7565d4e98de0747fb905556d238ff968
lunarinaa <aydan.akbarova@gmail.com>
2024-01-06 01:27:30 -0800

    number of internal nodes

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_nodes - function to count internal nodes
+ * @tree: pointer to the root node
+ * Return: number of internal nodes
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+       while (tree->left != NULL || tree->right != NULL)
+		return (1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+	return (0);
+}
+
